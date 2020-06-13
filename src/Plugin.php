bc3982be844f80d581655f5ab6a5474c84fd1b94
Jascha van Aalst <mr.jascha@gmail.com>
2020-06-14 01:55:49 +0200

    Plugin container implementation

--- src/Plugin.php
+++ src/Plugin.php
@@ -3,32 +3,39 @@
 namespace Jascha030\WP\Plugin\Core;
 
 use Jascha030\WP\Plugin\Core\Config\PluginConfig;
+use Jascha030\WP\Subscriptions\Exception\DoesNotImplementProviderException;
 use Jascha030\WP\Subscriptions\Exception\InvalidArgumentException;
 use Jascha030\WP\Subscriptions\Runnable\Runnable;
 use Jascha030\WP\Subscriptions\Shared\Container\Container;
 use Jascha030\WP\Subscriptions\Shared\Container\WordpressSubscriptionContainer;
 
+/**
+ * Class Plugin
+ *
+ * @package Jascha030\WP\Plugin\Core
+ */
 class Plugin extends Container implements Runnable
 {
     protected $pluginConfig;
 
     protected $pluginDir;
 
-    protected $mainClass;
-
     protected $ran = false;
+
+    protected $error;
 
     public function __construct(string $file)
     {
         $this->pluginDir = plugin_dir_path($file);
-        $bootstrapFile   = require($this->pluginDir);
 
-        if (! $bootstrapFile instanceof PluginConfig) {
-            throw new InvalidArgumentException("Wrong bootstrap directory: {$this->pluginDir}");
+        try {
+            $this->bootstrapPlugin();
+            $this->registerProviders();
+        } catch (InvalidArgumentException $e) {
+            // todo: error handling / logging
+        } catch (DoesNotImplementProviderException $e) {
+            // todo: error handling / logging
         }
-
-        $this->pluginConfig = $bootstrapFile;
-        $this->pluginConfig->run();
     }
 
     public static function define(string $name, $value): void
@@ -43,22 +50,61 @@ class Plugin extends Container implements Runnable
         return $this->pluginConfig->getPluginData();
     }
 
-    /**
-     * @throws \Jascha030\WP\Subscriptions\Exception\DoesNotImplementProviderException
-     */
     public function run(): void
     {
         if ($this->ran) {
             return;
         }
 
+        try {
+            // Hook all wordpress plugin actions / filters
+            WordpressSubscriptionContainer::getInstance()->run();
+            $this->ran = true;
+        } catch (\Exception $e) {
+            $this->error = $e;
+        }
+    }
+
+    public function getPluginProvider(): PluginProvider
+    {
+        return $this->resolve($this->pluginConfig->getMain());
+    }
+
+    protected function setPluginProvider(): void
+    {
+        $providerClass = $this->pluginConfig->getMain();
+        $provider      = new $providerClass($this->getConfig());
+
+        $this->bind($this->pluginConfig->getMain(), $provider);
+    }
+
+    /**
+     * @throws \Jascha030\WP\Subscriptions\Exception\InvalidArgumentException
+     */
+    protected function bootstrapPlugin(): void
+    {
+        $bootstrapFile = require $this->pluginDir . '/bootstrap.php';
+
+        if (! $bootstrapFile instanceof PluginConfig) {
+            throw new InvalidArgumentException("Wrong bootstrap directory: {$this->pluginDir}");
+        }
+
+        $this->pluginConfig = $bootstrapFile;
+        $this->pluginConfig->run();
+    }
+
+    /**
+     * @throws \Jascha030\WP\Subscriptions\Exception\DoesNotImplementProviderException
+     */
+    protected function registerProviders(): void
+    {
         $subscriptionContainer = WordpressSubscriptionContainer::getInstance();
         $providers             = $this->pluginConfig->getProviders();
+
+        $subscriptionContainer->register($this->getPluginProvider());
 
         foreach ($providers as $provider) {
             $subscriptionContainer->register($provider);
         }
-
-        $this->ran = true;
     }
 }
