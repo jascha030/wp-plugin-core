7843111ff5387930127f910ac6ce02f5eb8b8d24
Jascha van Aalst <mr.jascha@gmail.com>
2020-06-14 21:05:03 +0200

    docs

--- src/Config/Config.php
+++ src/Config/Config.php
@@ -4,6 +4,11 @@ namespace Jascha030\WP\Plugin\Core\Config;
 
 use Jascha030\WP\Subscriptions\Runnable\Runnable;
 
+/**
+ * Interface Config
+ *
+ * @package Jascha030\WP\Plugin\Core\Config
+ */
 interface Config extends Runnable
 {
 }
--- src/Config/PluginComponent.php
+++ src/Config/PluginComponent.php
@@ -4,6 +4,11 @@ namespace Jascha030\WP\Plugin\Core\Config;
 
 use Jascha030\WP\Subscriptions\Runnable\Runnable;
 
+/**
+ * Class PluginComponent
+ *
+ * @package Jascha030\WP\Plugin\Core\Config
+ */
 abstract class PluginComponent implements Runnable
 {
     protected $name;
